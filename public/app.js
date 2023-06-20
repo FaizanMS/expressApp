89b9ef903e714c82f34d729e162373b60780bdaf
Muhammad Faizan Alam <79311662+FaizanMS@users.noreply.github.com>
2023-06-20 23:30:19 +0500

    Finally Done

--- public/app.js
+++ public/app.js
@@ -1,11 +1,13 @@
-let getWeather = ()=>{
+window.getWeather = ()=>{
     let c_name = document.querySelector("#cityname").value
+    let lowerc_name = c_name.toLowerCase()
     let myApiKey = "a7dbc0c7fae4fc00acc8a74bea741742"
     let sirApiKey = "ecf07bafe5f67fa8a66abd728beb563b"
     axios.get(`/weather/${c_name}`)
     .then((res)=>{
        
 
+        // console.log(res.data)
         let cityname0 = c_name[0]
         let cityname1 = c_name.slice(1)
         let citynamecaps = cityname0.toUpperCase()
